2b2e68c23a6acb2ddb6971586343f8ac6fd5ac5a
Duality <201292@live.nl>
2015-08-31 03:42:19 +0200

    layout is not good, but will fix tommorow

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import sys
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 GRAY = (128, 128, 128)
 
 
@@ -12,6 +15,8 @@ class PianoKey(object):
         self.pos = pos
         self.width = width
         self.height = height
+        self.baseColor = WHITE
+        self.color = WHITE
         self.is_sharp = is_sharp
 
     def draw(self, screen):
@@ -19,30 +24,58 @@ class PianoKey(object):
         rect = (x, y, self.width, self.height)
         if self.is_sharp:
             pygame.draw.rect(screen, BLACK, rect, 0)
+            pygame.draw.rect(screen, GRAY, rect, 1)
         else:
             pygame.draw.rect(screen, WHITE, rect, 0)
-            pygame.draw.rect(screen, BLACK, rect, 2)
+            pygame.draw.rect(screen, BLACK, rect, 1)
+
+    def handleInput(self, input):
+        pass
+
+    def setpos(self, pos):
+        self.pos = pos
 
 
 class Octave(object):
-    def __init__(self, pos):
+    def __init__(self, pos, width=None, height=None):
         self.pos = pos
-        self.height = 100
+
         self.keyWidth = 20
         self.keyHeight = 100
+
         self.length = 8
+        if height:
+            self.height = height
+        else:
+            self.height = 100
+        if width:
+            self.width = width
+        else:
+            self.width = self.keyWidth * self.length
+
         self.keys = []
+        self.placekeys()
+
+    def placekeys(self):
+        # if keys already exist delete them
+        if len(self.keys):
+            self.keys = []
         # first add non sharp keys.
+        x, y = self.pos
         for i in range(0, self.length):
-            x, y = pos
             # keypos = (x, y + (i * self.keyWidth))
-            keypos = (i * self.keyWidth, y)
+            keypos = (x + (i * self.keyWidth), y)
             key = PianoKey(keypos, self.keyWidth, self.keyHeight, 0)
             self.keys.append(key)
-        # # second add sharp keys.
-        # for i in range(0, self.length):
-        #     x, y = pos
-        #     keypos = (i * self.keyWidth + 10)
+        # add sharp keys
+        skeywidth = self.keyWidth * 0.6
+        skeyheight = self.keyHeight / 3
+        # position is the position of key - half the sharp keys width.
+        for i in range(1, self.length):
+            if i != 3 and i != 4:
+                keypos = (x + (i * self.keyWidth - skeywidth / 2), y)
+                key = PianoKey(keypos, skeywidth, skeyheight, 1)
+                self.keys.append(key)
 
     def draw(self, screen):
         for key in self.keys:
@@ -51,12 +84,39 @@ class Octave(object):
         # skey.draw(screen)
         # pass
 
+    def handleInput(self, input):
+        pass
+
+    def getpos(self):
+        return self.pos
+
+    def getwidth(self):
+        return self.width
+
+    def getheight(self):
+        return self.height
+
+    def setpos(self, pos):
+        self.pos = pos
+        self.placekeys()
+
+    def setwidth(self, width):
+        self.width = width
+        self.placekeys()
+
+    def setheight(self, height):
+        self.height = height
+        self.placekeys()
+
 
 class Piano(object):
     def __init__(self, number_octaves):
         self.num_octaves = number_octaves
 
     def draw(self, screen):
+        pass
+
+    def handleInput(self, input):
         pass
 
 pygame.init()
@@ -71,6 +131,8 @@ key = PianoKey((10, 10), 20, 100, False)
 skey = PianoKey((20, 10), 20, 50, True)
 
 octave = Octave((0, 0))
+octave.setpos((0, height - octave.getheight()))
+octavetwo = Octave((octave.getwidth(), height - octave.getheight()))
 
 
 if __name__ == "__main__":
@@ -80,7 +142,7 @@ if __name__ == "__main__":
                 pygame.quit()
                 sys.exit(0)
             elif event.type == pygame.KEYDOWN:
-                pass
+                octave.handleInput(event)
             elif event.type == pygame.KEYUP:
                 pass
             elif event.type == pygame.MOUSEBUTTONDOWN:
@@ -88,5 +150,6 @@ if __name__ == "__main__":
 
         screen.fill(GRAY)
         octave.draw(screen)
+        octavetwo.draw(screen)
         pygame.display.flip()
         clock.tick(60)
